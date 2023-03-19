f80afce9b93795baf33d47dbdcb45300ff9d52a3
Bruno Duarte <duartebruno2208@gmail.com>
2023-03-18 22:35:55 -0300

    Initial commit

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,41 @@
+import { Header } from '../../components/Header';
+import { Summary } from '../../components/Summary';
+import { SearchForm } from './components/SearchForm';
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles';
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant='income'>R$12.00,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>18/03/2023</td>
+            </tr>
+            <tr>
+              <td width='50%'>Hamburguer</td>
+              <td>
+                <PriceHighlight variant='outcome'>- R$50,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>18/03/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  );
+}
